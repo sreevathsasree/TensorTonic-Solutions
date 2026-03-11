514f940c0444206eecefc02ad923ba874cc0f8ad
Sreevathsa1704 <140802159+sreevathsasree@users.noreply.github.com>
2026-03-11 15:45:53 +0530

    Add solution for anchor-box-generation

---
+++ anchor-box-generation/anchor-box-generation.py
@@ -0,0 +1,28 @@
+import math
+
+def generate_anchors(feature_size, image_size, scales, aspect_ratios):
+
+    anchors = []
+
+    stride = image_size / feature_size
+
+    for i in range(feature_size):
+        for j in range(feature_size):
+
+            cx = (j + 0.5) * stride
+            cy = (i + 0.5) * stride
+
+            for s in scales:
+                for r in aspect_ratios:
+
+                    w = s * math.sqrt(r)
+                    h = s / math.sqrt(r)
+
+                    x1 = cx - w/2
+                    y1 = cy - h/2
+                    x2 = cx + w/2
+                    y2 = cy + h/2
+
+                    anchors.append([x1, y1, x2, y2])
+
+    return anchors
